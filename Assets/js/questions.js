26ca94323e1830d452571f9feb1da7a33b9e2508
[Jessica Morse] <[jessicabrookemorse@gmail.com]>
2022-09-03 11:01:37 -0400

    added to JavaScript and HTML

--- Assets/js/questions.js
+++ Assets/js/questions.js
@@ -1,31 +1,31 @@
-var codeQuestions = [
-    {
-        question: "Which built-n method combines the text of two strings and returns a new string?"
-        answers: {
-        a: "append()"
-        b: "concat()"
-        c: "attach()"
-    },
-    correctAnswer: "b"
-}
-    {
-    question: "Which JavaScript label catches all the values, except for the ones specified?"
-        answers: {
-        a: "catch"
-        b: "label"
-        c: "default"
-
-    },
-correctAnswer: "c"
-    }
-{
-    question: "What will the following code return? Boolean(2<8)"
+export default codeQuestions = [
+  {
+    question:
+      "Which built-n method combines the text of two strings and returns a new string?",
     answers: {
-        a: "true"
-        b: "false"
-        c: "NaN"
+      a: "append()",
+      b: "concat()",
+      c: "attach()",
     },
-    correctAnswer: "a"
-}
-
-]
+    correctAnswer: "b",
+  },
+  {
+    question:
+      "Which JavaScript label catches all the values, except for the ones specified?",
+    answers: {
+      a: "catch",
+      b: "label",
+      c: "default",
+    },
+    correctAnswer: "c",
+  },
+  {
+    question: "What will the following code return? Boolean(2<8)",
+    answers: {
+      a: "true",
+      b: "false",
+      c: "NaN",
+    },
+    correctAnswer: "a",
+  },
+];
